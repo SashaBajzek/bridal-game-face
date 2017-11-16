876949eefa72d3a92cd3c01576ec31872d361883
Sasha Bajzek <dabridginator@gmail.com>
2017-11-15 21:18:43 -0800

    Make StopMotion functions more efficient

--- src/Components/StopMotion/StopMotion.js
+++ src/Components/StopMotion/StopMotion.js
@@ -9,8 +9,7 @@ import pause from '../../Photos/Icons/Pause.svg';
 class StopMotion extends Component {
 	state = {
 		currentItem: 0,
-		paused: true,
-		rampUp: false,
+		videoState: "paused",
 		totalItems: this.props.photos.length
 	};
 	
@@ -24,42 +23,33 @@ class StopMotion extends Component {
 	}
 	
 	timer = () => {
-		if(!this.state.paused){
-			if(this.state.currentItem < this.state.totalItems - 1){
-				return this.setState({currentItem: this.state.currentItem + 1})
-			} else {
-				return this.setState({currentItem: 0})
-			}
+		if(this.state.videoState !== "paused"){
+			const { currentItem, totalItems } = this.state;
+		 
+			this.setState({
+				currentItem: currentItem < totalItems - 1 ? currentItem + 1 : 0
+			});
 		}
 	}
 	
-	getPlayButtonClass = () => {
-		if(this.state.paused) {
-			return "StopMotion__toggle--paused";
-		}
-		if(this.state.rampUp){
-			return "StopMotion__toggle--playing StopMotion__toggle--rampUp";
-		}
-		return "StopMotion__toggle--playing";
+	rampUp = () => {
+		setTimeout(() => this.play(), 1000);
+		this.setState({videoState: "rampingUp"});
 	}
+	play = () => this.setState({videoState: "playing"});
+	pause = () => this.setState({videoState: "paused"});
 	
-	togglePlay = () => {
-		if (this.state.paused) {
-			this.setState({rampUp: true});
-			setTimeout(() => {
-				this.setState({rampUp: false})
-			}, 1000);
-		}
-		return this.setState({paused: !this.state.paused});
+	advanceVideoState = () => {
+		this.state.videoState === "paused" ? this.rampUp() : this.pause();
 	}
 
 	render() {
 		const { photos } = this.props;
-		const { currentItem, paused } = this.state;
+		const { currentItem, videoState } = this.state;
 		return (
 			<div className="StopMotion">
-				<img className="StopMotion__image" onClick={this.togglePlay} src={photos[currentItem].photo} alt="Testing in different lighting" />
-				<button className={`StopMotion__toggle ${this.getPlayButtonClass()}`} onClick={this.togglePlay}>{paused ? <img className="StopMotion__play" src={play} alt="Play video"/> : <img className="StopMotion__pause" src={pause} alt="Pause video"/>}</button>
+				<img className="StopMotion__image" onClick={this.advanceVideoState} src={photos[currentItem].photo} alt="Testing in different lighting" />
+				<button className={`StopMotion__toggle StopMotion__toggle--${videoState}`} onClick={this.advanceVideoState}>{videoState === "paused" ? <img className="StopMotion__play" src={play} alt="Play video"/> : <img className="StopMotion__pause" src={pause} alt="Pause video"/>}</button>
 				<p className="StopMotion__description">{photos[currentItem].description}</p>
 			</div>
 		)
