d60f0ac56a8ee913986630e7c293709acfc1420f
Sasha Bajzek <dabridginator@gmail.com>
2017-11-27 06:38:37 -0800

    BeforeAfter sliders responsive

--- src/Components/BeforeAfter/BeforeAfter.js
+++ src/Components/BeforeAfter/BeforeAfter.js
@@ -24,7 +24,7 @@ class BeforeAfter extends Component {
 		return <div className="BeforeAfter BeforeAfter__images">
 			<img src={afterImg} alt={`Face after ${product}`} />
 			<div className="BeforeAfter__image--before" style={{width: imageWidth}}>
-				<img src={beforeImg} alt={`Face after ${product}`} />
+				<img src={beforeImg} alt={`Face before ${product}`} />
 			</div>
 			<input type="range" min="0" max="100" value={this.state.value} name="image-slider" className="BeforeAfter__image-slider" onChange={this.handleChange} />
 		</div>
